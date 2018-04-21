7bf27e3ef2783c8e0dab4bfd573dd221a31cadf4
Tung Dao <me@tungdao.com>
2018-04-21 19:18:29 +0700

    add school app

--- schools/models.py
+++ schools/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class School(models.Model):
+    name = models.CharField(max_length=255)
+    website = models.URLField()
+    address = models.TextField()
+    postal_code = models.IntegerField()
+    phone_number = models.CharField(max_length=255)
+    fax_number = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    vision_statement = models.TextField()
+    mission_statement = models.TextField()
+    philosophy = models.TextField(null=True, blank=True)
+    dgp_code = models.CharField(max_length=255)
+    zone_code = models.CharField(max_length=255)
+    cluster_code = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+    session = models.CharField(max_length=255)
+    main_level = models.CharField(max_length=255)
+    language = models.CharField(max_length=255)
+    offer = models.TextField()
--- singapore_schools/settings.py
+++ singapore_schools/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'schools',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'singapore_schools.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('POSTGRES_DB', 'postgres'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
     }
 }
 
@@ -84,19 +88,15 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+AUTH_PASSWORD_VALIDATORS = [{
+    'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+}, {
+    'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+}, {
+    'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+}, {
+    'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+},
 ]
 
 
